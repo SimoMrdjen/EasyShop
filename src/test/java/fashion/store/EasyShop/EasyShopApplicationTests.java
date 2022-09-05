661d80a438161abb1b3a4b544433e008c0b2714a
Simo Mrdjen <dr.sizni@gmail.com>
2022-09-05 20:34:29 +0200

    Initial commit

---
+++ src/test/java/fashion/store/EasyShop/EasyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fashion.store.EasyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
