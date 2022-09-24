ac33da25c99aba77386669f716e46b6b4c8d98ae
Simo Mrdjen <dr.sizni@gmail.com>
2022-09-24 21:42:12 +0200

    added code for bundling front&backend, and making image"

--- src/main/java/fashion/store/EasyShop/contoller/CustomerController.java
+++ src/main/java/fashion/store/EasyShop/contoller/CustomerController.java
@@ -5,7 +5,15 @@ import fashion.store.EasyShop.service.implementation.CustomerService;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
