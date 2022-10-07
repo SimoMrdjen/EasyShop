a5d118cd0e5c7b7629026ad42698f7c81db73dcb
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-07 18:34:04 +0200

    addded lombok plugin

--- src/test/java/fashion/store/EasyShop/EasyShopApplicationTests.java
+++ src/test/java/fashion/store/EasyShop/EasyShopApplicationTests.java
@@ -3,7 +3,7 @@ package fashion.store.EasyShop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest//(classes = EasyShopApplicationTests.class)
+@SpringBootTest(classes = EasyShopApplicationTests.class)
 public
 class EasyShopApplicationTests {
 
