c4edb8668e71b7968eb8f7bd9630813ae16762a3
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-25 11:54:05 +0200

    Update InstallmentRepositoryTest.java

--- src/test/java/fashion/store/EasyShop/repository/InstallmentRepositoryTest.java
+++ src/test/java/fashion/store/EasyShop/repository/InstallmentRepositoryTest.java
@@ -27,29 +27,24 @@ class InstallmentRepositoryTest {
     PurchaseContract purchaseContract1;
     Customer customer;
 
-
     @BeforeEach
     void setUp() {
-
         customer = customerRepository.save( new Customer(null, "Mrdjen", "Simo", "0206970850101", "Yr",
                 "0205", "Zrenjanin PU", "dr.sizni@gmail.com", "0631030260"));
         purchaseContract = new PurchaseContract(null, customer, 100.00, 50.00, LocalDate.now(),
                 new ArrayList<Installment>());
         purchaseContract1 = new PurchaseContract(null, customer, 5.00, 2.00, LocalDate.now(),
                 new ArrayList<Installment>());
-        installment = repository.save(new Installment(purchaseContract, InstallmentOrdinal.FIRST, 20.00, LocalDate.now().plusMonths(1)));
-
-        installment2 = repository.save(new Installment(purchaseContract1, InstallmentOrdinal.FIRST, 1.00, LocalDate.now().plusMonths(1)));
+        installment = repository.save(new Installment(purchaseContract, InstallmentOrdinal.FIRST,
+                20.00, LocalDate.now().plusMonths(1)));
+        installment2 = repository.save(new Installment(purchaseContract1, InstallmentOrdinal.FIRST,
+                1.00, LocalDate.now().plusMonths(1)));
     }
 
     @Test
-    void findAllByCustomerId() {
+    void shouldreturnListWhenFindAllByPurchaseContract_Customer_Id() {
       List<Installment> insts = repository.findAllByPurchaseContract_Customer_Id(1L);
-        System.out.println();
-        insts.forEach(System.out::println);
-        //System.out.println(installment);
-
-        System.out.println();
-
+        assertThat(insts).usingDefaultElementComparator().
+                isEqualTo(List.of(installment,installment2));    
     }
 }
