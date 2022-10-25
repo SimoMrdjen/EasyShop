4b6b19e87f07c48d8e8994fba86112f92d030f50
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-25 12:14:39 +0200

    Add files via upload

---
+++ src/main/java/fashion/store/EasyShop/service/implementation/InstallmentService.java
@@ -0,0 +1,18 @@
+package fashion.store.EasyShop.service.implementation;
+
+import fashion.store.EasyShop.dto.InstallmentDto;
+import fashion.store.EasyShop.service.inter.IInstallmentService;
+
+import java.util.List;
+
+public class InstallmentService implements IInstallmentService {
+    @Override
+    public List<InstallmentDto> getAllInstallmentsByCustomerId(Long customerId) {
+        return null;
+    }
+
+    @Override
+    public InstallmentDto updateInstallment(InstallmentDto installmentDto) {
+        return null;
+    }
+}
