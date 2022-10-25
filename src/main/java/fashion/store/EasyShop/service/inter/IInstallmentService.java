6478cf4615ecb6961ae735955ec33b7309916330
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-25 12:06:26 +0200

    Update IInstallmentService.java
    interface added

--- src/main/java/fashion/store/EasyShop/service/inter/IInstallmentService.java
+++ src/main/java/fashion/store/EasyShop/service/inter/IInstallmentService.java
@@ -1,5 +1,7 @@
 package fashion.store.EasyShop.service.inter;
 
 public interface IInstallmentService {
-
+      List<InstallmentDto> getAllInstallmentsByCustomerId(Long customerId);
+    InstallmentDto updateInstallment(InstallmentDto installmentDto);
 }
+
