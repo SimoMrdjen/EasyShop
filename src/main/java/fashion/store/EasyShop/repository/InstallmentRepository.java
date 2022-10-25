998740c7db6e9f101b2e8a905bfaff580ed4c8ef
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-25 11:55:59 +0200

    Update InstallmentRepository.java
    changed test and InstallmentRepository

--- src/main/java/fashion/store/EasyShop/repository/InstallmentRepository.java
+++ src/main/java/fashion/store/EasyShop/repository/InstallmentRepository.java
@@ -6,19 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InstallmentRepository extends JpaRepository<Installment, Long> {
-
-//    @Query(value = "SELECT * FROM installment  JOIN\n" +
-//            "        purchase_contract  ON installment.purchase_contract_id = purchase_contract.id JOIN\n" +
-//            "        customer  ON purchase_contract.customer_id = customer.id\n" +
-//            "        WHERE customer.id = ?1 AND installment.paid_amount IS NULL", nativeQuery = true)
-//    List<Installment> findAllByCustomerId(Long customerId);
-
-//    @Query("select i from installment i where i.purchase_contract_id=?1 ")
-//    @Query("select i from installment i ")
-//    List<Installment> findSome();
-    List<Installment> findAllByPurchaseContractId(Long id);
+    
+    List<Installment> findAllByPurchaseContract_Id(Long id);
     List<Installment> findAllByPurchaseContract_Customer_Id(Long id);
-
-
-
 }
