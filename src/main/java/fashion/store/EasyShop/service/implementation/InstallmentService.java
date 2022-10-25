ae05e45a61691923b63132701e7071515e4a63ed
Simo Mrdjen <dr.sizni@gmail.com>
2022-10-25 14:36:32 +0200

    Update InstallmentService.java
    InstallmentSrervice added

--- src/main/java/fashion/store/EasyShop/service/implementation/InstallmentService.java
+++ src/main/java/fashion/store/EasyShop/service/implementation/InstallmentService.java
@@ -1,14 +1,33 @@
 package fashion.store.EasyShop.service.implementation;
 
 import fashion.store.EasyShop.dto.InstallmentDto;
+import fashion.store.EasyShop.mapper.InstallmentMapper;
+import fashion.store.EasyShop.repository.InstallmentRepository;
 import fashion.store.EasyShop.service.inter.IInstallmentService;
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class InstallmentService implements IInstallmentService {
+
+    @Autowired
+    private InstallmentRepository installmentRepository;
+    @Autowired
+    InstallmentMapper installmentMapper;
+
     @Override
     public List<InstallmentDto> getAllInstallmentsByCustomerId(Long customerId) {
-        return null;
+       return installmentRepository.findAllByPurchaseContract_Customer_Id(customerId).
+                stream().
+                filter(i -> Objects.nonNull(i.getPaidAmount())).
+                map(installmentMapper::mapGetEntityToDto).
+                collect(Collectors.toList());
+
     }
 
     @Override
